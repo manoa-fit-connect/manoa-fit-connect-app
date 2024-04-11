331c6f924aba6181f106ca9da4f41ef4c1eabc80
jerald <jeraldy@hawaii.edu>
2024-04-11 12:13:49 -1000

    Added Friends page and integrated it into the app with NavBar update

---
+++ app/imports/ui/pages/Friends.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+/** Render Friends Page */
+const Friends = () => (
+  <Container className="py-3">
+    <Row className="justify-content-center">
+      <Col xs={4} className="text-center">
+        <h2>
+          <p>Friends Page</p>
+        </h2>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Friends;
