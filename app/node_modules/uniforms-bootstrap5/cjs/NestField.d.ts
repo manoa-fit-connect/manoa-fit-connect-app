01d6dbef810fb426cff5a9748667918d86c05d8d
Hayden Bireley <156980200+hbireley@users.noreply.github.com>
2024-04-23 22:54:08 -1000

    Delete app/node_modules directory

--- app/node_modules/uniforms-bootstrap5/cjs/NestField.d.ts
+++
@@ -1,6 +0,0 @@
-import { HTMLFieldProps } from 'uniforms';
-export declare type NestFieldProps = HTMLFieldProps<object, HTMLDivElement, {
-    itemProps?: object;
-}>;
-declare const _default: import("uniforms").ConnectedField<NestFieldProps, object | undefined>;
-export default _default;
