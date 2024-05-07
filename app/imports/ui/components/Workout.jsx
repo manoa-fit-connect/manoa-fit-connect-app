d24807ce20295494b18b939e0be9541d864f713f
jerald <jeraldy@hawaii.edu>
2024-05-06 16:49:52 -1000

    Added Date conversion procedure pre-render

--- app/imports/ui/components/Workout.jsx
+++ app/imports/ui/components/Workout.jsx
@@ -4,39 +4,44 @@ import { Link } from 'react-router-dom';
 import { Card, Col, Row } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ProgressTracker.jsx. */
-const Workout = ({ workout }) => (
-  <Card>
-    <Card.Title>
-      <Row className="justify-content-center">
-        <Col className="text-center pt-2">
-          {workout.name}
-        </Col>
-      </Row>
-    </Card.Title>
-    <Card.Body>
-      <Row className="pb-3">
-        <Col>
-          <p className="fw-lighter">{workout.date.toLocaleDateString('en-US', { timeZone: 'GMT', day: 'numeric', month: 'numeric', year: 'numeric' })}</p>
-        </Col>
-        <Col>
-          Rating: {workout.rating}
-        </Col>
-        <Col>
-          Difficulty: {workout.difficulty}
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col className="text-center">
-          <h5>Highlight of the workout</h5>
-        </Col>
-      </Row>
-      {workout.highlight}
-      <Row>
-        <Link to={`/editWorkout/${workout._id}`} id="EditWorkout-Page">Edit</Link>
-      </Row>
-    </Card.Body>
-  </Card>
-);
+const Workout = ({ workout }) => {
+  // Ensure date is a Date object
+  const date = workout.date instanceof Date ? workout.date : new Date(workout.date);
+
+  return (
+    <Card>
+      <Card.Title>
+        <Row className="justify-content-center">
+          <Col className="text-center pt-2">
+            {workout.name}
+          </Col>
+        </Row>
+      </Card.Title>
+      <Card.Body>
+        <Row className="pb-3">
+          <Col>
+            <p className="fw-lighter">{date.toLocaleDateString('en-US', { timeZone: 'GMT', day: 'numeric', month: 'numeric', year: 'numeric' })}</p>
+          </Col>
+          <Col>
+            Rating: {workout.rating}
+          </Col>
+          <Col>
+            Difficulty: {workout.difficulty}
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col className="text-center">
+            <h5>Highlight of the workout</h5>
+          </Col>
+        </Row>
+        {workout.highlight}
+        <Row>
+          <Link to={`/editWorkout/${workout._id}`} id="EditWorkout-Page">Edit</Link>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+};
 
 // Require a document to be passed to this component.
 Workout.propTypes = {
